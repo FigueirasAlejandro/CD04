d450f90bfb232a0605f927d83b28940c6f86324e
Alejandro <Alejandro@DESKTOP-PU930BA>
2022-01-25 20:53:53 +0100

    prueba

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,4 +1,9 @@
 package cuentas;
+
+/**
+ * @author Alejandro
+ *
+ */
 public class CCuenta {
 
 
@@ -9,6 +14,12 @@ public class CCuenta {
     {
     }
 
+    /**
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -16,11 +27,18 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+    /**
+     * @return
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * @param cantidad
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -28,6 +46,10 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -37,26 +59,44 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+	/**
+	 * @return
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * @param cuenta
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * @return
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * @param saldo
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
